bab4005fdb8e0017e7c2fe2431899e51427c65c8
cz2542079957 <2542079957@qq.com>
2022-11-09 16:25:37 +0800

    start

---
+++ src/main/java/com/ahutoj/ahutojforum/AhutOjForumApplication.java
@@ -0,0 +1,15 @@
+package com.ahutoj.ahutojforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhutOjForumApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(AhutOjForumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ahutoj/ahutojforum/AhutOjForumApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ahutoj.ahutojforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhutOjForumApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
